10b988e2352df1122c5ba8a9598041f111ce56a5
AMaoDEEP23 <151827609+AMaoDEEP23@users.noreply.github.com>
2024-03-25 23:32:21 +0700

    update3

--- Client/src/Header.jsx
+++ Client/src/Header.jsx
@@ -7,7 +7,7 @@ export default function Header(){
     return (
         <div>
              <header className=" flex justify-between">
-        <a href="" className="flex item-center gap-1">
+        <Link to={'/'} className="flex item-center gap-1">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -23,7 +23,7 @@ export default function Header(){
             />
           </svg>
           <span className="font-bold text-xl">AirBooking</span>
-        </a>
+        </Link>
         <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
           <div>Anywhere</div>
           <div className="border border-l boder-gray-300"></div>
