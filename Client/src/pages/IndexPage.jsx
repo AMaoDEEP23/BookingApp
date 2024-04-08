15c4db6c6f160d7478e9a10035b8a85ae702967b
AMaoDEEP23 <151827609+AMaoDEEP23@users.noreply.github.com>
2024-04-08 15:38:37 +0700

    update5

--- Client/src/pages/IndexPage.jsx
+++ Client/src/pages/IndexPage.jsx
@@ -1,17 +1,19 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
+import { Link } from "react-router-dom";
 
 export default function IndexPage (){
     const [places, setPlaces] = useState([])
     useEffect(() => {
         axios.get('/places').then(response => {
-            setPlaces([...response.data, ...response.data, ...response.data, ...response.data]);
+            setPlaces(response.data);
         })
     }, []);
     return(
         <div className="mt-8 grid gap-x-6 gap-y-8 grid-cols-4 md:grid-cols-3 lg:grid-cols-4">
             {places.length> 0 && places.map(place =>(
                 <>
+                <Link to={'/place/'+place._id}>
                 <div>
                     <div className="bg-gray-500 mb-2 rounded-2xl flex ">
                         {place.photos?.[0] &&(
@@ -24,6 +26,7 @@ export default function IndexPage (){
                         <span className="font-bold">${place.price} per night</span>
                     </div>
                 </div>
+                </Link>
                 </>
             ))}
         </div>
